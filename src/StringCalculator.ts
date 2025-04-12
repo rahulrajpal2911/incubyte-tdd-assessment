a043668ed02abacc9fa7717a86128adb89b94b82
Rahul Rajpal <rahulrajpal2911@gmail.com>
2025-04-12 16:09:19 +0530

    Added one scenario to test, function must exist.

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,4 +1,12 @@
-export function calculateString() {
-  console.log("Reached function");
-  return true;
+export function calculateString(str: string) {
+  if (str.length !== 0) {
+    if (!isNaN(Number(str)) && Number(str) < 10) {
+      return Number(str);
+    } else {
+      const regex = /[,]/g;
+      const numbers = str.split(regex).map(Number);
+      return numbers.reduce((a, b) => a + b);
+    }
+  }
+  return 0;
 }
--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -2,6 +2,27 @@ import { calculateString } from "../src/StringCalculator";
 
 describe("String Calculator TDD Kata", () => {
   it("function should exist", () => {
-    expect(calculateString()).toBeDefined();
+    expect(calculateString("0")).toBeDefined();
+  });
+
+  //An empty string returns `0`.
+  it("Should return `0` if empty string found", () => {
+    expect(calculateString("0")).toBe(0);
+  });
+
+  //A single number returns the number itself.
+  it("Should return number if it's single digit", () => {
+    for (let i = 0; i < 10; i++) {
+      expect(calculateString(i.toString())).toBe(i);
+    }
+  });
+
+  //Two numbers, comma-delimited, return their sum.
+  it("Should return sum if delimiter is a comma", () => {
+    for (let i = 0; i < 10; i++) {
+      expect(calculateString(`${i.toString()},${(i + 1).toString()}`)).toBe(
+        i + (i + 1)
+      );
+    }
   });
 });
